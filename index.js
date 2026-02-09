7aa5527b6d946e1097ddfb502075c0d01cff44d5
Luis <luischacon0423@gmail.com>
2026-02-09 04:51:19 -0800

    nuevo proyecto 1

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const fn = () => {
     let b = miString + " muy feliz"
     console.log(b)
 }
-
+ 
 fn()
 
 const clickeado = () => {
-    let a = "chanchito feliz"
+    let a = "luis feliz"
     let b = a + " muy feliz" 
 
     console.log(b)
 }
 
 const clickeado2 = () => {
-    let a = "chanchito triste"
+    let a = "luis triste"
     let b = a + " muy triste" 
 
     console.log(b)
